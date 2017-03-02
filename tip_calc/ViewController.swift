f403f8f39918a924bd514c0ea8529e6b7dbc2db0
Deniz Tumer <e.tumer.deniz@gmail.com>
2017-03-01 23:06:35 -0800

    Initial Commit

---
+++ tip_calc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tip_calc
+//
+//  Created by Deniz Tumer on 3/1/17.
+//  Copyright © 2017 Deniz Tumer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
